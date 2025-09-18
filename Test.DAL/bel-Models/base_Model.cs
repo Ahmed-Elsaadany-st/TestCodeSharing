5c859a602d8a8d1b85afca7002da89904b452ad7
Belal <BelalGit@gmail.com>
2025-09-18 14:31:26 +0300

    Belal_Test

---
+++ Test.DAL/bel-Models/base_Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.DAL.bel_Models
+{
+    public class base_Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public int createdby { get; set; }
+        public int LastModifiedby { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
